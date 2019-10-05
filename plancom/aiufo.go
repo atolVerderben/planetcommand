102261e240990f3763abe967cac55ffa05338ab4
Brian Stair <brianstair@gmail.com>
2019-10-05 09:59:20 -0500

    Start work on UFO enemy

--- plancom/aiufo.go
+++ plancom/aiufo.go
@@ -7,10 +7,30 @@ import "github.com/atolVerderben/tentsuyu"
 //AIUfo controls the ufo that attacks the player planet
 type AIUfo struct {
 	*tentsuyu.BasicObject
-	Health int
+	Health      int
+	Destination *tentsuyu.Vector2d
+	anim        *tentsuyu.Animation
+}
+
+//SpawnUFO at the provided coords
+func SpawnUFO(x, y float64) *AIUfo {
+	u := &AIUfo{
+		BasicObject: tentsuyu.NewBasicObject(x, y, 32, 32),
+		Destination: &tentsuyu.Vector2d{X: -1, Y: -1},
+	}
+	return u
 }
 
 //Update the AIUfo
-func (u *AIUfo) Update() {
+func (u *AIUfo) Update(p *Planet) {
+	if !u.hasDestination() {
+		//Get a new destination to move to
+	}
+}
 
+func (u *AIUfo) hasDestination() bool {
+	if u.Destination.X == -1 && u.Destination.Y == -1 {
+		return false
+	}
+	return true
 }
